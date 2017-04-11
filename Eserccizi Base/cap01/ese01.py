10088e51f0de435759a720384c2d2c5030509b99
cosprin <cost.prin@gmail.com>
2017-04-11 20:34:39 +0200

    Risolto esercizio 1

--- Eserccizi Base/cap01/ese01.py
+++ Eserccizi Base/cap01/ese01.py
@@ -1,12 +1,8 @@
-#!/bin/env python
-#
-# Testo :
-# Scrivere/eseguire un programma visualizzi su schermo la scritta “Hello world!”.
-#
-#
+
+
 
 def main ( ) :
-    # todo: scrivvere qui il programma
+    print ("Hello world")
     return
 
 if __name__ == "ese01" :
